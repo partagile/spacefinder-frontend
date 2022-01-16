0dc544f6d68217bd4075de340705ff3b7002b18f
Thushyanthan A <hello@thushyanthan.com>
2022-01-16 15:24:30 -0800

    upload file to s3 bucket

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -1,16 +1,43 @@
 import { ICreateSpaceState } from "../components/spaces/CreateSpace";
-import { SpaceComponent } from "../components/spaces/SpaceComponent";
 import { Space } from "../model/Model";
+import { S3, config } from "aws-sdk";
+import { config as appConfig } from "./config";
 
-
+config.update({
+    region: appConfig.REGION
+})
 
 export class DataService {
-    
-    public async createSpaces(ICreateSpace: ICreateSpaceState){
+
+    private s3Client = new S3({
+        region: appConfig.REGION
+    })
+
+    public async createSpaces(iCreateSpace: ICreateSpaceState) {
+        if (iCreateSpace.photo) {
+            const photoUrl = await this.uploadPublicFile(
+                iCreateSpace.photo,
+                appConfig.SPACES_PHOTOS_BUCKET
+            )
+            console.log(photoUrl)
+        }
         return 'testing'
     }
-    
-    public async getSpaces(): Promise<Space[]>{
+
+    private async uploadPublicFile(file: File, bucket: string) {
+        const fileName = file.name;
+        const uploadResult = await new S3({ region: appConfig.REGION }).upload({
+            Bucket: bucket,
+            Key: fileName,
+            Body: file,
+            ACL: 'public-read'
+
+        }).promise();
+        return uploadResult.Location
+
+    }
+
+    public async getSpaces(): Promise<Space[]> {
         const result: Space[] = []
         result.push({
             location: 'Prince George',
@@ -30,10 +57,10 @@ export class DataService {
         return result
     }
 
-    
-    public async reserveSpace(spaceId: string):Promise<string | undefined> {
+
+    public async reserveSpace(spaceId: string): Promise<string | undefined> {
         if (spaceId === '123') {
-            return('5555')
+            return ('5555')
         } else {
             return undefined
         }
