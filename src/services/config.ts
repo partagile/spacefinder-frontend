3ce1e98afa51467aff5825378d49503ea12ad909
Thushyanthan A <hello@thushyanthan.com>
2022-01-18 15:18:24 -0800

    redeploy new stack

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,12 +1,12 @@
 
-const spacesUrl = 'https://qoe8b3qb2k.execute-api.us-west-2.amazonaws.com/prod/'
+const spacesUrl = 'https://q68651sk71.execute-api.us-west-2.amazonaws.com/prod/'
 
 export const config = {
     REGION: 'us-west-2',
-    USER_POOL_ID: 'us-west-2_fBM44XXy9',
-    APP_CLIENT_ID: '5psn11jv28hloisnss1opcne6u',
-    IDENTITY_POOL_ID: 'us-west-2:5cc69f83-0552-465e-8a4f-ee2968530367',
-    SPACES_PHOTOS_BUCKET: 'spacefinderz-photos-024df7423f65',
+    USER_POOL_ID: 'us-west-2_ntoyPoIN1',
+    APP_CLIENT_ID: '6otvml2idkamc9e2pn6l19q9lv',
+    IDENTITY_POOL_ID: 'us-west-2:d033c78d-9b5f-434c-a9f9-fe8186f915a5',
+    SPACES_PHOTOS_BUCKET: 'spacefinderz-photos-0abb7079cdb5',
     TEST_USER_NAME: 'hello',
     TEST_USER_PASSWORD: '5EApCsq&',
     api: {
